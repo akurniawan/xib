13299c2d9116c54cd54d0456071d905baeba3340
akurniawan <akurniawan.cs@gmail.com>
2021-05-27 08:15:52 +0200

    try to remove initial saving

--- xib/training/manager.py
+++ xib/training/manager.py
@@ -5,14 +5,13 @@ from abc import ABC, abstractmethod
 
 import torch
 import torch.nn as nn
-from torch.optim import SGD, Adagrad, Adam
-from torch.optim.lr_scheduler import CyclicLR
-
 from dev_misc import g
 from dev_misc.arglib import add_argument, init_g_attr
 from dev_misc.trainlib import Metrics, has_gpus, set_random_seeds
 from dev_misc.trainlib.trainer import freeze
 from dev_misc.utils import deprecated
+from torch.optim import SGD, Adagrad, Adam
+from torch.optim.lr_scheduler import CyclicLR
 from xib.data_loader import (ContinuousTextDataLoader, DataLoaderRegistry,
                              DenseIpaDataLoader, IpaDataLoader)
 from xib.model.decipher_model import DecipherModel
@@ -28,14 +27,19 @@ from xib.training.task import (AdaptCbowTask, AdaptLMTask, CbowTask,
 from xib.training.trainer import (AdaptLMTrainer, DecipherTrainer,
                                   ExtractTrainer, LMTrainer)
 
-add_argument('task', default='lm', dtype=str, choices=[
-             'lm', 'cbow', 'adapt_lm', 'adapt_cbow', 'decipher', 'search', 'extract'], msg='which task to run')
+add_argument(
+    "task",
+    default="lm",
+    dtype=str,
+    choices=["lm", "cbow", "adapt_lm", "adapt_cbow", "decipher", "search", "extract"],
+    msg="which task to run",
+)
 
 
 class BaseManager(ABC):
-
     @abstractmethod
-    def run(self): ...
+    def run(self):
+        ...
 
 
 class LMManager(BaseManager):
@@ -54,10 +58,15 @@ class LMManager(BaseManager):
         self.dl_reg = DataLoaderRegistry()
         self.dl_reg.register_data_loader(task, g.data_path)
         self.evaluator = LMEvaluator(self.model, self.dl_reg[task])
-        self.trainer = self.trainer_cls(self.model, [task], [1.0], 'total_step',
-                                        evaluator=self.evaluator,
-                                        check_interval=g.check_interval,
-                                        eval_interval=g.eval_interval)
+        self.trainer = self.trainer_cls(
+            self.model,
+            [task],
+            [1.0],
+            "total_step",
+            evaluator=self.evaluator,
+            check_interval=g.check_interval,
+            eval_interval=g.eval_interval,
+        )
 
     def run(self):
         self.trainer.train(self.dl_reg)
@@ -84,14 +93,13 @@ class AdaptCbowManager(CbowManager):
 
 class DecipherManager(BaseManager):
 
-    add_argument('dev_data_path', dtype='path', msg='Path to dev data.')
-    add_argument('aux_train_data_path', dtype='path', msg='Path to aux train data.')
-    add_argument('in_domain_dev_data_path', dtype='path', msg='Path to in-domain dev data.')
-    add_argument('saved_path', dtype='path')
-    add_argument('saved_model_path', dtype='path', msg='Path to a saved model, skipping the local training phase.')
-    add_argument('train_phi', dtype=bool, default=False,
-                 msg='Flag to train phi score. Used only with supervised mode.')
-    add_argument('fix_phi', dtype=bool, default=False, msg='Flag fix phi scorer.')
+    add_argument("dev_data_path", dtype="path", msg="Path to dev data.")
+    add_argument("aux_train_data_path", dtype="path", msg="Path to aux train data.")
+    add_argument("in_domain_dev_data_path", dtype="path", msg="Path to in-domain dev data.")
+    add_argument("saved_path", dtype="path")
+    add_argument("saved_model_path", dtype="path", msg="Path to a saved model, skipping the local training phase.")
+    add_argument("train_phi", dtype=bool, default=False, msg="Flag to train phi score. Used only with supervised mode.")
+    add_argument("fix_phi", dtype=bool, default=False, msg="Flag fix phi scorer.")
     # add_argument('use_mlm_loss', dtype=bool, default=False, msg='Flag to use mlm loss.')
 
     def __init__(self):
@@ -99,17 +107,17 @@ class DecipherManager(BaseManager):
         if has_gpus():
             self.model.cuda()
 
-        train_task = DecipherTask('train')
-        dev_task = DecipherTask('dev')
+        train_task = DecipherTask("train")
+        dev_task = DecipherTask("dev")
         self.dl_reg = DataLoaderRegistry()
         eval_tasks = [train_task, dev_task]
         if g.in_domain_dev_data_path:
-            in_domain_dev_task = DecipherTask('in_domain_dev')
+            in_domain_dev_task = DecipherTask("in_domain_dev")
             self.dl_reg.register_data_loader(in_domain_dev_task, g.in_domain_dev_data_path)
             eval_tasks.append(in_domain_dev_task)
         train_tasks = [train_task]
         if g.aux_train_data_path:
-            aux_train_task = DecipherTask('aux_train')
+            aux_train_task = DecipherTask("aux_train")
             self.dl_reg.register_data_loader(aux_train_task, g.aux_train_data_path)
             train_tasks.append(aux_train_task)
 
@@ -117,10 +125,15 @@ class DecipherManager(BaseManager):
         self.dl_reg.register_data_loader(dev_task, g.dev_data_path)
         self.evaluator = DecipherEvaluator(self.model, self.dl_reg, eval_tasks)
 
-        self.trainer = DecipherTrainer(self.model, train_tasks, [1.0] * len(train_tasks), 'total_step',
-                                       evaluator=self.evaluator,
-                                       check_interval=g.check_interval,
-                                       eval_interval=g.eval_interval)
+        self.trainer = DecipherTrainer(
+            self.model,
+            train_tasks,
+            [1.0] * len(train_tasks),
+            "total_step",
+            evaluator=self.evaluator,
+            check_interval=g.check_interval,
+            eval_interval=g.eval_interval,
+        )
         if g.train_phi:
             freeze(self.model.self_attn_layers)
             freeze(self.model.positional_embedding)
@@ -130,10 +143,10 @@ class DecipherManager(BaseManager):
             self.trainer.load(g.saved_model_path, load_phi_scorer=True)
             if g.fix_phi:
                 freeze(self.model.phi_scorer)
-        #     freeze(self.model.self_attn_layers)
-        #     freeze(self.model.positional_embedding)
-        #     freeze(self.model.emb_for_label)
-        #     freeze(self.model.label_predictor)
+            #     freeze(self.model.self_attn_layers)
+            #     freeze(self.model.positional_embedding)
+            #     freeze(self.model.emb_for_label)
+            #     freeze(self.model.label_predictor)
             self.trainer.set_optimizer()
 
     def run(self):
@@ -166,11 +179,11 @@ class SearchSolverManager(BaseManager):
 
     def run(self):
         dl_reg = DataLoaderRegistry()
-        dev_task = DecipherTask('dev')
+        dev_task = DecipherTask("dev")
         dl_reg.register_data_loader(dev_task, g.dev_data_path)
         dl = dl_reg[dev_task]
 
-        with open(g.vocab_path, 'r', encoding='utf8') as fin:
+        with open(g.vocab_path, "r", encoding="utf8") as fin:
             vocab = set(line.strip() for line in fin)
 
         solver = SearchSolver(vocab, g.max_num_words)
@@ -190,20 +203,20 @@ class ReduceLR:
     def step(self):
         """Copied from https://pytorch.org/docs/stable/_modules/torch/optim/lr_scheduler.html#ReduceLROnPlateau."""
         for i, param_group in enumerate(self.optimizer.param_groups):
-            old_lr = float(param_group['lr'])
+            old_lr = float(param_group["lr"])
             new_lr = old_lr * self._factor
-            param_group['lr'] = new_lr
-            logging.imp(f'Learning rate is now {new_lr:.4f}.')
+            param_group["lr"] = new_lr
+            logging.imp(f"Learning rate is now {new_lr:.4f}.")
 
 
 class ExtractManager(BaseManager):
 
     # IDEA(j_luo) when to put this in manager/trainer? what about scheduler? annealing? restarting? Probably all in trainer -- you need to track them with pbars.
-    add_argument('optim_cls', default='adam', dtype=str, choices=['adam', 'adagrad', 'sgd'], msg='Optimizer class.')
-    add_argument('anneal_factor', default=0.5, dtype=float, msg='Mulplication value for annealing.')
-    add_argument('min_threshold', default=0.01, dtype=float, msg='Min value for threshold')
+    add_argument("optim_cls", default="adam", dtype=str, choices=["adam", "adagrad", "sgd"], msg="Optimizer class.")
+    add_argument("anneal_factor", default=0.5, dtype=float, msg="Mulplication value for annealing.")
+    add_argument("min_threshold", default=0.01, dtype=float, msg="Min value for threshold")
 
-    _name2cls = {'adam': Adam, 'adagrad': Adagrad, 'sgd': SGD}
+    _name2cls = {"adam": Adam, "adagrad": Adagrad, "sgd": SGD}
 
     def __init__(self):
         task = ExtractTask()
@@ -211,7 +224,7 @@ class ExtractManager(BaseManager):
         self.dl_reg.register_data_loader(task, g.data_path)
 
         lu_size = None
-        if g.input_format == 'text':
+        if g.input_format == "text":
             lu_size = self.dl_reg[task].dataset.unit_vocab_size
         self.model = ExtractModel(lu_size=lu_size)
         if has_gpus():
@@ -219,12 +232,17 @@ class ExtractManager(BaseManager):
 
         self.evaluator = ExtractEvaluator(self.model, self.dl_reg[task])
 
-        self.trainer = ExtractTrainer(self.model, [task], [1.0], 'total_step',
-                                      stage_tnames=['round', 'total_step'],
-                                      evaluator=self.evaluator,
-                                      check_interval=g.check_interval,
-                                      eval_interval=g.eval_interval,
-                                      save_interval=g.save_interval)
+        self.trainer = ExtractTrainer(
+            self.model,
+            [task],
+            [1.0],
+            "total_step",
+            stage_tnames=["round", "total_step"],
+            evaluator=self.evaluator,
+            check_interval=g.check_interval,
+            eval_interval=g.eval_interval,
+            save_interval=g.save_interval,
+        )
         if g.saved_model_path:
             self.trainer.load(g.saved_model_path)
         # self.trainer.set_optimizer(Adam, lr=g.learning_rate)
@@ -235,11 +253,12 @@ class ExtractManager(BaseManager):
         self.trainer.set_optimizer(optim_cls, lr=g.learning_rate)  # , momentum=0.9, nesterov=True)
         # Save init parameters.
 
-        out_path = g.log_dir / f'saved.init'
-        self.trainer.save_to(out_path)
+        out_path = g.log_dir / f"saved.init"
+        # self.trainer.save_to(out_path)
         while self.trainer.threshold > g.min_threshold:
             self.trainer.reset()
             self.trainer.set_optimizer(optim_cls, lr=g.learning_rate)
 
             self.trainer.train(self.dl_reg)
-            self.trainer.tracker.update('round')
+            self.trainer.tracker.update("round")
+
