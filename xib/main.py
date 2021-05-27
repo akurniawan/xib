ce2ebdaff9819871590c50cebf44ff58351f65de
akurniawan <akurniawan.cs@gmail.com>
2021-05-27 07:31:57 +0200

    remove commit_id

--- xib/main.py
+++ xib/main.py
@@ -1,31 +1,37 @@
 import torch
-
 from dev_misc.arglib import g, parse_args, show_args
 from dev_misc.devlib import initiate
 from dev_misc.devlib.named_tensor import patch_named_tensors
 from dev_misc.trainlib import set_random_seeds
+
 from xib.cfg import reg
-from xib.training.manager import (AdaptCbowManager, AdaptLMManager,
-                                  BaseManager, CbowManager, DecipherManager,
-                                  ExtractManager, LMManager,
-                                  SearchSolverManager)
+from xib.training.manager import (
+    AdaptCbowManager,
+    AdaptLMManager,
+    BaseManager,
+    CbowManager,
+    DecipherManager,
+    ExtractManager,
+    LMManager,
+    SearchSolverManager,
+)
 
 
 def train():
     manager: BaseManager
-    if g.task == 'lm':
+    if g.task == "lm":
         manager = LMManager()
-    elif g.task == 'cbow':
+    elif g.task == "cbow":
         manager = CbowManager()
-    elif g.task == 'adapt_cbow':
+    elif g.task == "adapt_cbow":
         manager = AdaptCbowManager()
-    elif g.task == 'adapt':
+    elif g.task == "adapt":
         manager = AdaptLMManager()
     # elif g.task == 'transfer':
     #     manager = TransferManager()
-    elif g.task == 'search':
+    elif g.task == "search":
         manager = SearchSolverManager()
-    elif g.task == 'extract':
+    elif g.task == "extract":
         manager = ExtractManager()
     else:
         manager = DecipherManager()
@@ -33,7 +39,7 @@ def train():
 
 
 if __name__ == "__main__":
-    initiate(reg, logger=True, log_dir=True, log_level=True, gpus=True, random_seed=True, commit_id=True)
+    initiate(reg, logger=True, log_dir=True, log_level=True, gpus=True, random_seed=True, commit_id=False)
     patch_named_tensors()
 
     # IDEA(j_luo) Set random seed here?
